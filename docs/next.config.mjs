9a9ab75bc14198fa94f43ed915c835d4e7f2d967
Sagar Velankar <Sagar Velankar>
2024-08-30 17:43:40 +0530

    update nextra

---
+++ docs/next.config.mjs
@@ -0,0 +1,20 @@
+import nextra from 'nextra'
+
+const withNextra = nextra({
+  theme: 'nextra-theme-docs',
+  themeConfig: './theme.config.tsx',
+  latex: true,
+  flexsearch: {
+    codeblocks: false
+  },
+  defaultShowCopyCode: true
+});
+
+export default withNextra({
+  basePath: '/architecture',
+  output: 'export',
+  images: {
+    unoptimized: true,
+  },
+  reactStrictMode: true
+})
